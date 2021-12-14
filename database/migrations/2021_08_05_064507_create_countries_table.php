6b88da406105631b02d667a2c5f6485689bfdc0a
shafeeqAhmed <shafeeq.ahmed541@gmail.com>
2021-12-14 23:31:58 +0500

    Create 2021_08_05_064507_create_countries_table.php

---
+++ database/migrations/2021_08_05_064507_create_countries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->string('country_id')->primary();
+            $table->string('country_name')->nullable();
+            $table->string('country_sort_name')->nullable();
+            $table->integer('country_phone_code')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
