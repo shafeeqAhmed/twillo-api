888b0800412af1ffa7e356a601fd276c7a9b96ea
Talha Fayyaz <talha.fayyaz52@gmail.com>
2021-12-16 17:30:19 +0500

    InfluencerController db transection

--- app/Http/Controllers/Api/InfluencerController.php
+++ app/Http/Controllers/Api/InfluencerController.php
@@ -6,17 +6,23 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\updateInfluencer;
 use App\Models\User;
+use App\Models\TwilioNumbers;
+use Illuminate\Support\Facades\DB;
 
 class InfluencerController extends Controller
 {
    public function updateInfluencer(updateInfluencer $request){
         
       try {
-
+     
+      DB::beginTransaction();
      User::updateUser('user_uuid',$request['user_uuid'],$request->except('user_uuid'));
-                    
+     TwilioNumbers::updateTwilo('id',$request['twilo_id'],['status'=>'inactive']); 
+      DB::commit();
+
             return response()->json(['status' => true, 'message' => 'User has been updated successfully', 'data' => []]);
         } catch (Exception $e) {
+             DB::rollBack();
             return response()->json(['status' => false, 'message' => $e->getMessage(), 'data' => []]);
         }
 
