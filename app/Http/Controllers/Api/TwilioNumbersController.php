dcc618ab250ec2ea15c807c65e1cb2ea0d416e41
Muhammad Talha <talha.fayyaz52@gmail.com>
2021-12-24 00:49:12 +0500

    changes

--- app/Http/Controllers/Api/TwilioNumbersController.php
+++ app/Http/Controllers/Api/TwilioNumbersController.php
@@ -94,51 +94,7 @@ class TwilioNumbersController extends ApiController
         ]);
     }
 
-        public function smsService(Request $request){
-          
-       
-        /*$message=$this->client->messages
-                  ->create($request->receiver_number,
-                           ["body" => $request->message, "from" =>  $request->user()->phone_no, "statusCallback" => "https://text-app.tkit.co.uk/api/api/twilio_webhook"]
-                  );*/
      
-       DB::transaction(function() use ($request){
-
-    $chat_users=ChatUsers::where(['sender_id'=>$request->user()->id,'receiver_id'=>$request->receiver_id])->orWhere(['receiver_id'=>$request->user()->id,'sender_id'=>$request->receiver_id])->get();
-           
-
-        if( count($chat_users)==0){
-            $chat_user_record=ChatUsers::create([
-            'sender_id'=>$request->user()->id,
-            'receiver_id'=>$request->receiver_id,
-            'is_active'=>1,
-             'type'=> 'one-to-one'
-           ]);
-
-        $chat_user_id=$chat_user_record->id;
-        }else{
-          $chat_user_id=$chat_users[0]->id;
-        }
-
-
-         $message_record=Messages::create([
-           'sms_uuid'=>Str::uuid()->toString(),
-           'sender_id'=>$request->user()->id,
-           'receiver_id'=>$request->receiver_id,
-            'message_id'=>0,
-            'message'=>$request->message,
-            'is_seen'=>0,
-            'chat_user_id'=>$chat_user_id
-          ]);
-
-          return $this->respond([
-            'data' => $message_record->id
-        ]);
-
-          });
-
-
-    }
     
     
     public function twilioWebhook(){
@@ -186,20 +142,7 @@ class TwilioNumbersController extends ApiController
         
     }
 
-    public function getChatUsers(Request $request,$id){
 
-     $sender_id=$request->user()->id;
-     $receiver_id=$id;
-
-     $messages=ChatUsers::with('chat_messages.user')->where(['sender_id'=>$sender_id,'receiver_id'=>$receiver_id])->first();
-
-
-          
-        return $this->respond([
-        'data' =>  new ChatUserResource($messages)
-        ]);
-
-    }
 
 
 }
