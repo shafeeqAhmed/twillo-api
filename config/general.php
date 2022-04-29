765cf81e4edba7f7399b9fdebd3b94792c00e7b7
shafeeqAhmed <shafeeq.ahmed541@gmail.com>
2022-04-30 04:06:40 +0500

    update new build

--- config/general.php
+++ config/general.php
@@ -5,6 +5,6 @@ return [
     'twilio_sid' => env('TWILIO_SID'),
     'twilio_token' => env('TWILIO_TOKEN'),
     'front_app_url' => env('FRONT_END_APP', 'http://localhost:3000'),
-    'web_hook' => env('WEB_HOOK', 'https://text-app.tkit.co.uk/twillo-api/api/twilio_webhook'),
+    'web_hook' => env('WEB_HOOK', 'https://colony.rocks/twillo-api/api/twilio_webhook'),
 
 ];
