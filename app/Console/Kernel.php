f6e529642663f1f89611c5167f0ab101208221fa
shafeeqAhmed <shafeeq.ahmed541@gmail.com>
2022-05-01 20:03:09 +0500

    cronjob for queue work

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('queue:work --daemon')->everyMinute()->withoutOverlapping();
         // $schedule->command('inspire')->hourly();
     }
 
@@ -34,7 +35,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
